2d1623b842e18b6fd6ad750af5451cabff9f7f73
Meilinea <kathleen.stassart@gmail.com>
2020-03-19 10:59:46 +0100

    barre de progression formulaire diagnostic

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,239 @@ require('bootstrap');
 // import $ from 'jquery';
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//    $("#next-1").click(function(){
+//     $("#second").show();
+//     $("#first").hide();
+//     $("#progressBar").css("width","60%");
+//     $("#progressbarText").html("Etape 2");
+
+//    });
+//    $("#next-2").click(function(){
+//     $("#third").show();
+//     $("#second").hide();
+//     $("#progressBar").css("width","100%");
+//     $("#progressbarText").html("Step - 3");
+//    });
+//    $("#prev-2").click(function(){
+//     $("#second").hide();
+//     $("#first").show();
+//     $("#progressBar").css("width","20%");
+//     $("#progressbarText").html("Step - 1");
+//    });
+//    $("#prev-3").click(function(){
+//     $("#second").show();
+//     $("#third").hide();
+//     $("#progressBar").css("width","60%");
+//     $("#progressbarText").html("Step - 2");
+//    });
+// });
+
+
+var i = 1;
+$('.progress .circle').removeClass().addClass('circle');
+$('.progress .bar').removeClass().addClass('bar');
+setInterval(function() {
+  $('.progress .circle:nth-of-type(' + i + ')').addClass('active');
+  //pour le cercle bleu
+
+  $('.progress .circle:nth-of-type(' + i + ') .label').html('&#46;&#46;&#46;');
+  
+  $('.progress .circle:nth-of-type(' + (i-1) + ')').removeClass('active').addClass('done');
+  //Pour le vert
+  
+  $('.progress .circle:nth-of-type(' + (i-1) + ') .label').html('&#10003;');
+  //10003 correspond au V
+  
+//   $('.progress .bar:nth-of-type(' + (i-1) + ')').addClass('active');
+  
+//   $('.progress .bar:nth-of-type(' + (i-2) + ')').removeClass('active').addClass('done');
+  
+  i++;
+  
+  
+}, 1000);
+
+
